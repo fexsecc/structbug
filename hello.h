18403d202f2c7173c14ca7399a76946fb16ff1ae
fexsec <165517769+fexsecc@users.noreply.github.com>
2025-07-20 14:39:46 +0300

    Added header parsing into DWARF

---
+++ hello.h
@@ -0,0 +1,7 @@
+struct hacker {
+    char* alias;
+    char** hacks;
+    int hacksCnt;
+};
+
+typedef struct hacker hacker;
--- structbug.py
+++ structbug.py
@@ -3,59 +3,59 @@ import argparse
 import os
 import sys
 
-#def run_tilib():
-#    # Versions starting with IDA 9.1 come with tilib out of the box
-#    # If missing, install it manually
-#    # Then add tilib to PATH
-#    import subprocess
-#
-#    proc = subprocess.Popen(["cat", "/etc/passwd"], stdout=subprocess.PIPE, shell=True)
-#    (out, err) = proc.communicate()
-#    print("program output:", out)
-#
-#def extract_til():
-#    # Extract .til local types section from the idb/i64 file
-#    with open(sys.argv[2][:-3] + "til", "rb") as db:
-#        data = db.read()
-#        start = data.find(b"IDATIL")
-#        db.seek(start)
-#        end = data.find(b"IDAS")
-#        til_size = end - start
-#        til = db.read(til_size)
-#        with open("./tmp.til", "wb") as o:
-#          o.write(til)
-#
-#
-#def clean_ida_header():
-#    # Remove default IDA declarations as I cant find __offset
-#    # Anywhere and have no idea what it is
-#    
-#    with open(sys.argv[1], "r") as f:
-#      lines = f.readlines()
-#    
-#    exc_start = -1
-#    exc_end = -1
-#    for i in range(len(lines)):
-#      if '/* 1 */\n' == lines[i]:
-#        exc_start = i
-#      elif '/* 7 */\n' == lines[i]:
-#        exc_end = i + 3
-#
-#    if exc_start == -1 or exc_end == -1:
-#        return
-#    
-#    with open(sys.argv[1], "w") as o:
-#      for i in range(len(lines)):
-#        if i < exc_start or i > exc_end:
-#          o.write(lines[i])
-#
-#
-#def produce_dwarf():
-#    with open("./tmp.cpp", "w") as src:
-#        src.write(f'#include "{sys.argv[1]}"\n')
-#    os.system("g++ -o tmp tmp.cpp -g3 -c -fno-eliminate-unused-debug-types -fno-eliminate-unused-debug-symbols -O0")
-#    os.system(f"objcopy --only-keep-debug tmp {sys.argv[1][:-2]}.debug")
-#    os.system("rm -f tmp tmp.c")
+def run_tilib():
+    # Versions starting with IDA 9.1 come with tilib out of the box
+    # If missing, install it manually
+    # Then add tilib to PATH
+    import subprocess
+
+    proc = subprocess.Popen(["cat", "/etc/passwd"], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    print("program output:", out)
+
+def extract_til():
+    # Extract .til local types section from the idb/i64 file
+    with open(sys.argv[2][:-3] + "til", "rb") as db:
+        data = db.read()
+        start = data.find(b"IDATIL")
+        db.seek(start)
+        end = data.find(b"IDAS")
+        til_size = end - start
+        til = db.read(til_size)
+        with open("./tmp.til", "wb") as o:
+          o.write(til)
+
+
+def clean_ida_header(header):
+    # Remove default IDA declarations as I cant find __offset
+    # Anywhere and have no idea what it is
+    
+    with open(header, "r") as f:
+      lines = f.readlines()
+    
+    exc_start = -1
+    exc_end = -1
+    for i in range(len(lines)):
+      if '/* 1 */\n' == lines[i]:
+        exc_start = i
+      elif '/* 7 */\n' == lines[i]:
+        exc_end = i + 3
+
+    if exc_start == -1 or exc_end == -1:
+        return
+    
+    with open(sys.argv[1], "w") as o:
+      for i in range(len(lines)):
+        if i < exc_start or i > exc_end:
+          o.write(lines[i])
+
+
+def produce_dwarf(header, output_name):
+    with open("./tmp.cpp", "w") as src:
+        src.write(f'#include "{header}"\n')
+    os.system("g++ -o tmp tmp.cpp -g3 -c -fno-eliminate-unused-debug-types -fno-eliminate-unused-debug-symbols -O0")
+    os.system(f"objcopy --only-keep-debug tmp {output_name}")
+    os.system("rm -f tmp tmp.cpp")
 
 
 def main():
@@ -109,18 +109,21 @@ def main():
         print(f"Unexpected error: {e}")
         exit(1)
 
-    print(args)
-    #elif len(sys.argv) == 2:
-    #    clean_ida_header()
-    #    produce_dwarf()
-    #elif len(sys.argv) == 3:
-    #     TODO: implement i64 extraction of db
-    #    extract_til()
-    #    run_tilib()
-    #else:
-    #    print("[X] Invalid options given")
-    #    print("USAGE: ./structbug.py <header> <optional i64 DB file>")
-    #    exit(1)
+    if args.header:
+        # In case the header is still taken from IDA,
+        # Get rid of unwanted default types
+        clean_ida_header(args.header)
+
+        if args.format == 'dwarf':
+            produce_dwarf(args.header, args.output)
+        elif args.format == 'pdb':
+            # TODO: Implement pdb creation
+            #produce_pdb()
+            exit(1)
+    elif args.i64:
+        exit(1)
+        extract_til()
+        run_tilib()
 
 if __name__ == '__main__':
     main()
